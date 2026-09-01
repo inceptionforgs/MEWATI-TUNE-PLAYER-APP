224706f4bdbad4f9d09b8c273ed37da0044c87fd
Proton-email <experimentforpii@proton.me>
2026-09-01 23:20:52 +0530

    Create route_names.dart

---
+++ lib/routes/route_names.dart
@@ -0,0 +1,6 @@
+class RouteNames {
+  static const String splash = '/';
+  static const String home = '/home';
+  static const String singerProfile = '/singer-profile';
+  static const String nowPlaying = '/now-playing';
+}
