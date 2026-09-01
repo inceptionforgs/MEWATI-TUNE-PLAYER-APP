a4303703c8cd623783e59ddcd165df4d4ab10c0b
Proton-email <experimentforpii@proton.me>
2026-09-01 21:35:02 +0530

    Create app_dimensions.dart

---
+++ lib/core/constants/app_dimensions.dart
@@ -0,0 +1,24 @@
+class AppDimensions {
+  static const double paddingXS = 4.0;
+  static const double paddingS = 8.0;
+  static const double paddingM = 12.0;
+  static const double paddingL = 16.0;
+  static const double paddingXL = 20.0;
+  static const double paddingXXL = 24.0;
+
+  static const double radiusS = 8.0;
+  static const double radiusM = 12.0;
+  static const double radiusL = 16.0;
+  static const double radiusXL = 24.0;
+
+  static const double iconSizeS = 16.0;
+  static const double iconSizeM = 24.0;
+  static const double iconSizeL = 32.0;
+
+  static const double tabHeight = 53.0;
+  static const double miniPlayerHeight = 64.0;
+
+  static const double albumArtSize = 230.0;
+  static const double avatarSize = 54.0;
+  static const double smallAvatarSize = 28.0;
+}
