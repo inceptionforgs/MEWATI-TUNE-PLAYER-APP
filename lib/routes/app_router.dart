18606ec773e6e91928d6c8565a5d8ee7f9626a4f
Proton-email <experimentforpii@proton.me>
2026-09-02 23:18:55 +0530

    Update app_router.dart

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -4,26 +4,43 @@ import '../screens/splash/splash_screen.dart';
 import '../screens/home/home_screen.dart';
 import '../screens/singers/singer_profile_screen.dart';
 import '../screens/player/now_playing_screen.dart';
+import 'route_names.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => const SplashScreen());
-      case '/home':
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
-      case '/singer-profile':
+      case RouteNames.splash:
+        return MaterialPageRoute(
+          builder: (_) => const SplashScreen(),
+          settings: settings,
+        );
+      case RouteNames.home:
+        return MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
+          settings: settings,
+        );
+      case RouteNames.singerProfile:
         final singer = settings.arguments;
         if (singer is! Singer) {
-          return MaterialPageRoute(builder: (_) => const HomeScreen());
+          return MaterialPageRoute(
+            builder: (_) => const HomeScreen(),
+            settings: settings,
+          );
         }
         return MaterialPageRoute(
           builder: (_) => SingerProfileScreen(singer: singer),
+          settings: settings,
         );
-      case '/now-playing':
-        return MaterialPageRoute(builder: (_) => const NowPlayingScreen());
+      case RouteNames.nowPlaying:
+        return MaterialPageRoute(
+          builder: (_) => const NowPlayingScreen(),
+          settings: settings,
+        );
       default:
-        return MaterialPageRoute(builder: (_) => const SplashScreen());
+        return MaterialPageRoute(
+          builder: (_) => const SplashScreen(),
+          settings: settings,
+        );
     }
   }
 }
