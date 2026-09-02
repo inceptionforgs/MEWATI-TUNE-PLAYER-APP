2fd52caaa94439784972939f45de72f148e9955c
Proton-email <experimentforpii@proton.me>
2026-09-02 08:45:57 +0530

    Update sleep_timer_provider.dart

--- lib/providers/sleep_timer_provider.dart
+++ lib/providers/sleep_timer_provider.dart
@@ -5,9 +5,11 @@ class SleepTimerProvider extends ChangeNotifier {
   final SleepTimerService _sleepTimerService = SleepTimerService();
 
   Duration? _remaining;
+  Duration? _totalDuration;
   bool _isFading = false;
 
   Duration? get remaining => _remaining;
+  Duration? get totalDuration => _totalDuration;
   bool get isActive => _sleepTimerService.isActive;
   bool get isFading => _isFading;
 
@@ -16,16 +18,19 @@ class SleepTimerProvider extends ChangeNotifier {
       duration,
       onTick: () {
         _remaining = _sleepTimerService.remaining;
+        _totalDuration = _sleepTimerService.totalDuration;
         _isFading = _sleepTimerService.isFading;
         notifyListeners();
       },
       onComplete: () {
         _remaining = Duration.zero;
+        _totalDuration = Duration.zero;
         _isFading = false;
         notifyListeners();
       },
     );
     _remaining = duration;
+    _totalDuration = duration;
     _isFading = false;
     notifyListeners();
   }
@@ -33,6 +38,7 @@ class SleepTimerProvider extends ChangeNotifier {
   void cancel() {
     _sleepTimerService.cancel();
     _remaining = null;
+    _totalDuration = null;
     _isFading = false;
     notifyListeners();
   }
