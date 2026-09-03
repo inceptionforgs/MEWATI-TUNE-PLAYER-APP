95688eed515ae766439d2e54dbeff782104afba3
Proton-email <experimentforpii@proton.me>
2026-09-03 06:51:52 +0530

    Update brand_row.dart

--- lib/screens/home/widgets/brand_row.dart
+++ lib/screens/home/widgets/brand_row.dart
@@ -1,6 +1,7 @@
+// File: lib/screens/home/widgets/brand_row.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../../core/constants/app_themes.dart';
 import '../../../providers/theme_provider.dart';
 
 class BrandRow extends StatelessWidget {
