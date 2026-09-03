6d3ff77bac1f2dc1f48b66903fad61162ab8fc63
Proton-email <experimentforpii@proton.me>
2026-09-03 09:06:29 +0530

    Update route_names.dart

--- lib/routes/route_names.dart
+++ lib/routes/route_names.dart
@@ -3,4 +3,8 @@ class RouteNames {
   static const String home = '/home';
   static const String singerProfile = '/singer-profile';
   static const String nowPlaying = '/now-playing';
+  static const String feedback = '/feedback';
+  static const String advanceSettings = '/advance-settings';
+  static const String driveMode = '/drive-mode';
+  static const String search = '/search';
 }
