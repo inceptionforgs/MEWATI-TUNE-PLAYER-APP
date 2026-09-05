1c80edf508729d0c20bd1c0f24e76096fece3fc8
Proton-email <experimentforpii@proton.me>
2026-09-05 20:51:36 +0530

    Update singer_profile_screen.dart

--- lib/screens/singers/singer_profile_screen.dart
+++ lib/screens/singers/singer_profile_screen.dart
@@ -1,3 +1,5 @@
+// File: lib/screens/singers/singer_profile_screen.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -33,8 +35,9 @@ class _SingerProfileScreenState extends State<SingerProfileScreen> {
   @override
   void initState() {
     super.initState();
-    _loadSongs();
-    Future.microtask(() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      _loadSongs();
       Provider.of<FavoritesProvider>(context, listen: false).loadFavorites();
     });
   }
